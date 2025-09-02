6a2bbf089e5cefaf70a7f2f66b56cd99af1a7497
Priyabrato <sahpriyabrato@gmail.com>
2025-09-03 00:00:30 +0530

    Add initial client and server setup with Bun, including React and Tailwind integration

---
+++ server/src/index.ts
@@ -0,0 +1,18 @@
+import express from "express";
+import cors from "cors";
+
+
+const app = express();
+
+
+app.use(express.json());
+app.use(cors());
+
+app.get("/", (req, res) => {
+    res.send("Server is running");
+});
+
+const PORT = process.env.PORT || 5001;
+app.listen(PORT, () => {
+    console.log(`Server is running on port: ${PORT}`);
+})
