5fcc960c6a3ba34e032b71eb91393e35228cfc1e
Priyabrato <sahpriyabrato@gmail.com>
2025-09-23 21:21:52 +0530

    Refactor Home and Sidebar components for improved structure and styling. Update Home component to display "Home" text and enhance Sidebar layout with session management and SignoutButton integration.

--- client/src/components/home/sidebar.tsx
+++ client/src/components/home/sidebar.tsx
@@ -1,83 +1,98 @@
-"use client"
-import React from 'react'
+"use client";
+import React from "react";
 import {
-    Sidebar,
-    SidebarContent,
-    SidebarFooter,
-    SidebarGroup,
-    SidebarGroupContent,
-    SidebarMenu,
-    SidebarMenuButton,
-    SidebarMenuItem,
-} from "@/components/ui/sidebar"
+  Sidebar,
+  SidebarContent,
+  SidebarFooter,
+  SidebarGroup,
+  SidebarGroupContent,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+} from "@/components/ui/sidebar";
 
-import Link from 'next/link'
-import { CreditCardIcon, Folder, Home, Settings } from 'lucide-react'
-import { Avatar, AvatarFallback, AvatarImage } from '../ui/avatar'
-import { useSession } from 'next-auth/react'
-import SignoutButton from '../login/Signout-button'
-
+import Link from "next/link";
+import { CreditCardIcon, Folder, Home, Settings } from "lucide-react";
+import { Avatar, AvatarFallback, AvatarImage } from "../ui/avatar";
+import { useSession } from "next-auth/react";
+import SignoutButton from "../login/Signout-button";
 
 const items = [
-    {
-        title: "Home",
-        icon: "Home",
-        href: "/",
-        active: true,
-    },
-    {
-        title: "Projects",
-        icon: "Folder",
-        href: "/home/projects",
-        active: false,
-    },
-    {
-        title: "Billing",
-        icon: "CreditCardIcon",
-        href: "/billing",
-        active: false,
-    },
-    {
-        title: "Settings",
-        icon: "Settings",
-        href: "/settings",
-        active: false,
-    }
-]
+  {
+    title: "Home",
+    icon: "Home",
+    href: "/home",
+    active: false,
+  },
+  {
+    title: "Projects",
+    icon: "Folder",
+    href: "/home/projects",
+    active: false,
+  },
+  {
+    title: "Billing",
+    icon: "CreditCardIcon",
+    href: "/billing",
+    active: false,
+  },
+  {
+    title: "Settings",
+    icon: "Settings",
+    href: "/home/settings",
+    active: false,
+  },
+];
 export const HomeSidebar = () => {
-    const { data: session } = useSession();
+  const { data: session } = useSession();
 
-    return (
-        <Sidebar variant="floating" collapsible="none" className="h-screen bg-indigo-600 w-min flex flex-col rounded-r-xl  text-2xl font-mono">
-            <SidebarContent>
-                <SidebarGroup>
-                    <SidebarGroupContent>
-                        <SidebarMenu className='w-min text-white rounded-lg p-1'>
-                            {items.map((item) => (
-                                <SidebarMenuItem key={item.icon} >
-                                    <SidebarMenuButton asChild size={'lg'} isActive={item.active}>
-                                        <Link href={`${item.href}`} className='flex flex-col justify-center items-center h-max rounded-md focus:bg-indigo-400 transition-colors mb-1'>
-                                            {item.icon === "Home" && <Home className='w-12 h-12' />}
-                                            {item.icon === "Folder" && <Folder className='w-12 h-12 ' />}
-                                            {item.icon === "CreditCardIcon" && <CreditCardIcon className='w-12 h-12' />}
-                                            {item.icon === "Settings" && <Settings className='w-12 h-12 ' />}
-                                            <span>{item.title}</span>
-                                        </Link>
-                                    </SidebarMenuButton>
-                                </SidebarMenuItem>
-                            ))}
-                        </SidebarMenu>
-                    </SidebarGroupContent>
-                </SidebarGroup>
-            </SidebarContent>
+  return (
+    <Sidebar
+      variant="floating"
+      collapsible="none"
+      className="h-auto bg-indigo-600 w-min flex flex-col rounded-lg mx-1 my-1 text-2xl font-mono"
+    >
+      <SidebarContent>
+        <SidebarGroup>
+          <SidebarGroupContent>
+            <SidebarMenu className="w-min text-white rounded-lg p-1">
+              {items.map((item) => (
+                <SidebarMenuItem key={item.icon}>
+                  <SidebarMenuButton asChild size={"lg"} isActive={item.active}>
+                    <Link
+                      href={`${item.href}`}
+                      className="flex flex-col justify-center items-center h-max rounded-md focus:bg-indigo-400 transition-colors mb-1"
+                    >
+                      {item.icon === "Home" && <Home className="w-12 h-12" />}
+                      {item.icon === "Folder" && (
+                        <Folder className="w-12 h-12 " />
+                      )}
+                      {item.icon === "CreditCardIcon" && (
+                        <CreditCardIcon className="w-12 h-12" />
+                      )}
+                      {item.icon === "Settings" && (
+                        <Settings className="w-12 h-12 " />
+                      )}
+                      <span>{item.title}</span>
+                    </Link>
+                  </SidebarMenuButton>
+                </SidebarMenuItem>
+              ))}
+            </SidebarMenu>
+            <div className="flex justify-end items-end flex-col gap-y-4 h-full mr-2">
+              <SignoutButton />
+              <Avatar className="h-12 w-12">
+                <AvatarImage
+                  src={session?.user?.image || "https://github.com/shadcn.png"}
+                />
+                <AvatarFallback>{session?.user?.name}</AvatarFallback>
+              </Avatar>
+            </div>
+          </SidebarGroupContent>
+        </SidebarGroup>
+      </SidebarContent>
 
-            <SidebarFooter>
-                <SignoutButton />
-                <Avatar className='h-12 w-12 ml-3'>
-                    <AvatarImage src={session?.user?.image || "https://github.com/shadcn.png"} />
-                    <AvatarFallback>{session?.user?.name}</AvatarFallback>
-                </Avatar>
-            </SidebarFooter>
-        </Sidebar>
-    )
-}
+      <SidebarFooter></SidebarFooter>
+    </Sidebar>
+  );
+};
--- client/src/components/login/Signout-button.tsx
+++ client/src/components/login/Signout-button.tsx
@@ -5,24 +5,26 @@ import { signOut } from "next-auth/react";
 import { LucideLogOut } from "lucide-react";
 
 const SignoutButton = () => {
+  const handleSignout = async () => {
+    try {
+      await signOut({ redirect: false, callbackUrl: "/login" }); // prevent auto-redirect
+      toast("Logged out successfully!");
+    } catch (err) {
+      console.error("Error signing out:", err);
+      toast("Failed to log out");
+    }
+  };
 
-    const handleSignout = async () => {
-
-        try {
-            await signOut({ redirect: false, callbackUrl: "/login" }); // prevent auto-redirect
-            toast("Logged out successfully!");
-        } catch (err) {
-            console.error("Error signing out:", err);
-            toast("Failed to log out");
-        }
-    };
-
-    return (
-        <button onClick={handleSignout} className="flex flex-col items-center py-1 px-0 text-red-700 bg-red-300 justify-center p-0 text-sm font-medium rounded-xl hover:bg-red-500 opacity-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500">
-            <LucideLogOut className="h-5 w-5 rotate-180 text-red-700" />
-            <span>logout</span>
-        </button>
-    );
+  return (
+    <>
+      <button
+        onClick={handleSignout}
+        className="flex flex-col items-center p-4 text-white bg-red-400 justify-center text-sm font-medium rounded-xl hover:bg-red-300 "
+      >
+        <LucideLogOut className="h-5 w-5" />
+      </button>
+    </>
+  );
 };
 
 export default SignoutButton;
